2867f4f2b6d0e9362ce4c6481c98ddbaa67af19a
Samuel Stone <samuel.w.stone@gmail.com>
2024-04-23 01:26:54 -0400

    php compiled via script

--- servant_viewer.php
+++ servant_viewer.php
@@ -10,7 +10,7 @@ if (isset($_GET['servant'])) {
 
 global $wpdb;
 
-$queries = array("name", "align", "str", "end", "agi", "man", "lck", "mrs", "class", "off", "quali", "place", "pfp", "war", "player", "np", "type", "anti", "quote");
+$queries = array("name", "align", "str", "end", "agi", "man", "lck", "mrs", "class", "off", "quali", "place", "pfp", "war", "player", "np", "type", "anti", "quote", "weak");
 
 for ($i = 0; $i < count($queries); $i++) {
     ${$queries[$i]} = $wpdb->get_var("SELECT $queries[$i] FROM Servants WHERE tag='$id'");
@@ -18,75 +18,77 @@ for ($i = 0; $i < count($queries); $i++) {
 
 echo "
 <div class = 'gold'>
-  <div class = 'goldblack'>
-    <div class = 'charsheet'>
-      <div class = 'column'>
-        <div class = 'row border top'>
-          <h2 class = 'title'>$name</h2>
-          <h4>Played by $player</h4>
-          <h5>Placed $place in War $war</h5>
-        </div>
-        <div class ='row border'>
-          <h3 class ='class-attribute' style='float: left'>Class:</h3>
-          <h3 style ='float: left'>$class</h3>
-          <h4 class = 'off'>$off</h3>
-          <h4 class ='class-attribute attr-row' style = 'clear: both; float: left'>Alignment:</h4>
-          <h4 class = 'attr-row' style ='float: left'> $align</h4>
-          <h5 class ='class-attribute attr-row' style = 'clear: both; float: left'>Qualified Classes:</h5>
-          <h5 class = 'attr-row' style ='float: left'>$quali</h5>
-        </div>
-        <div class = 'row border'>
-          <div style = 'float: left'>
-            <table class='attr-table'>
-              <tr>
-                <th class = 'attribute'>Strength: </th>
-                <th>$str</th>
-              </tr>
-              <tr>
-                <th class = 'attribute'>Agility: </th>
-                <th>$agi</th>
-              </tr>
-              <tr>
-                <th class = 'attribute'>Magic: </th>
-                <th>$man</th>
-              </tr>
-            </table>
+    <div class = 'goldblack'>
+      <div class = 'charsheet'>
+        <div class = 'column'>
+          <div class = 'row border top'>
+            <h2 class = 'title'>$name</h2>
+            <h4>Played by $player</h4>
+            <h5>Placed $place in War $war</h5>
           </div>
-          <div style = 'float:right'>
-            <table>
-              <tr>
-                <th class = 'attribute'>Endurance: </th>
-                <th>$end</th>
-              </tr>
-              <tr>
-                <th class = 'attribute'>Luck: </th>
-                <th>$lck</th>
-              </tr>
-              <tr>
-                <th class = 'attribute'>Magic Res: </th>
-                <th>$mrs</th>
-              </tr>
-            </table>
+          <div class ='row border'>
+            <h3 class ='class-attribute' style='float: left'>Class:</h3>
+            <h3 style ='float: left'>$class</h3>
+            <h4 class = 'off'>$off</h3>
+            <h4 class ='class-attribute attr-row' style = 'clear: both; float: left'>Alignment:</h4>
+            <h4 class = 'attr-row' style ='float: left'> $align</h4>
+            <h5 class ='class-attribute attr-row' style = 'clear: both; float: left'>Strongly Qualified Classes:</h5>
+            <h5 class = 'attr-row' style ='float: left'>$quali</h5>
+            <h5 class ='class-attribute attr-row' style = 'clear: both; float: left'>Weakly Qualified Classes:</h5>
+            <h5 class = 'attr-row' style ='float: left'>$weak</h5>
+          </div>
+          <div class = 'row border'>
+            <div style = 'float: left'>
+              <table class='attr-table'>
+                <tr>
+                  <th class = 'attribute'>Strength: </th>
+                  <th>$str</th>
+                </tr>
+                <tr>
+                  <th class = 'attribute'>Agility: </th>
+                  <th>$agi</th>
+                </tr>
+                <tr>
+                  <th class = 'attribute'>Magic: </th>
+                  <th>$man</th>
+                </tr>
+              </table>
+            </div>
+            <div style = 'float:right'>
+              <table>
+                <tr>
+                  <th class = 'attribute'>Endurance: </th>
+                  <th>$end</th>
+                </tr>
+                <tr>
+                  <th class = 'attribute'>Luck: </th>
+                  <th>$lck</th>
+                </tr>
+                <tr>
+                  <th class = 'attribute'>Magic Res: </th>
+                  <th>$mrs</th>
+                </tr>
+              </table>
+            </div>
+          </div>
+          <div class = 'row border bottom'>
+            <h3 class = 'title'>$np</h3>
+            <h4>Type: $type Anti-$anti</h4>
           </div>
         </div>
-        <div class = 'row border bottom'>
-          <h3 class = 'title'>Noble Phantasm</h3>
-          <h3>$np</h3>
-          <h4>Type: $type Anti-$anti</h4>
+        <div class = 'column'>
+          <img class = 'servant-pfp border' src = '$pfp'>
+          <div class = 'rowright border bottom'>
+            <h5>\"$quote\"</h5>
+          </div>
         </div>
+        <div class = 'shimmer'></div>
       </div>
-      <div class = 'column'>
-        <img class = 'servant-pfp border' src = '$pfp'>
-        <div class = 'rowright border bottom'>
-          <h5>\"$quote\"</h5>
-        </div>
-      </div>
-      <div class = 'shimmer'></div>
     </div>
   </div>
-</div>
 ";
 ?>
+
 <style>
 @import url('https://fonts.cdnfonts.com/css/luminari');
 
@@ -109,14 +111,6 @@ echo "
   border-color: lightgray;
   border-width: 2px;
   border-style: solid;
-}
-
-.triple {
-  border-radius:5px;
-  border-color: white;
-  border-width: 2px;
-  border-style: solid;
-  margin: 0px 5px 0px 5px;
 }
 
 .gold {
